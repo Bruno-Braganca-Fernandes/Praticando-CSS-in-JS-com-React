6c71daa861583676657e1fefe69900ccef959687
bruno <bruno.b.fernandes12@hotmail.com>
2025-06-03 23:22:14 -0300

    Styling the sidebar and finishing the project

--- src/components/Projeto/index.tsx
+++ src/components/Projeto/index.tsx
@@ -1,15 +1,15 @@
 import Paragrafo from '../Paragrafo'
 import Titulo from '../Titulo'
 
-import {Card, LinkBotao} from './styles'
+import { Card, LinkBotao } from './styles'
 
 const Projeto = () => {
   return (
-  <Card>
-    <Titulo>Projeto lista de tarefas</Titulo>
-    <Paragrafo tipo="secundario">Lista de tarefas feita com VueJs</Paragrafo>
-    <LinkBotao>Visualizar</LinkBotao>
-  </Card>
+    <Card>
+      <Titulo>Projeto lista de tarefas</Titulo>
+      <Paragrafo tipo="secundario">Lista de tarefas feita com VueJs</Paragrafo>
+      <LinkBotao>Visualizar</LinkBotao>
+    </Card>
   )
 }
 
